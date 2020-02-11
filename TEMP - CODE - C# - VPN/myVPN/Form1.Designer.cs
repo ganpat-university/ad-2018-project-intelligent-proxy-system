0ecf77826040500f1f62cf6fd905c7f0f94730e4
Soumya Mukherjee <56750696+soumyagnu@users.noreply.github.com>
2020-02-11 23:25:20 +0530

    Add files via upload

---
+++ TEMP - CODE - C# - VPN/myVPN/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace myVPN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.txtUsrname = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel2.Controls.Add(this.Label2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Label4, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.txtHost, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.txtPassword, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.Label3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.txtUsrname, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 1, 3);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(321, 236);
+            this.tableLayoutPanel2.TabIndex = 14;
+            // 
+            // Label2
+            // 
+            this.Label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(29, 23);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(32, 13);
+            this.Label2.TabIndex = 16;
+            this.Label2.Text = "Host:";
+            // 
+            // Label4
+            // 
+            this.Label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(8, 141);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(53, 13);
+            this.Label4.TabIndex = 17;
+            this.Label4.Text = "Password";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtHost.Location = new System.Drawing.Point(67, 19);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(246, 20);
+            this.txtHost.TabIndex = 17;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtPassword.Location = new System.Drawing.Point(67, 137);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(246, 20);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // Label3
+            // 
+            this.Label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(3, 82);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(58, 13);
+            this.Label3.TabIndex = 18;
+            this.Label3.Text = "Username:";
+            // 
+            // txtUsrname
+            // 
+            this.txtUsrname.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtUsrname.Location = new System.Drawing.Point(67, 78);
+            this.txtUsrname.Name = "txtUsrname";
+            this.txtUsrname.Size = new System.Drawing.Size(246, 20);
+            this.txtUsrname.TabIndex = 10;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.btnDisconnect, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnConnect, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(67, 180);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(251, 53);
+            this.tableLayoutPanel3.TabIndex = 19;
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(128, 3);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 15;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(3, 3);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 13;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 236);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My VPN";
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.TextBox txtHost;
+        internal System.Windows.Forms.TextBox txtPassword;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox txtUsrname;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        internal System.Windows.Forms.Button btnDisconnect;
+        internal System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ TEMP - CODE - C# - VPN/myVPN/bin/Debug/vpnconnector/VpnConnection.bat
@@ -0,0 +1,1 @@
+rasdial "VPN" vpnbook 6b467gr /phonebook:"E:\aco\myVPN-master\myVPN\bin\Debug\vpnconnector\VpnConnection.pbk"

bf283b9ca92cd731484b44977fbf2524fab61933
Soumya Mukherjee <56750696+soumyagnu@users.noreply.github.com>
2020-04-01 00:39:20 +0530

    Add files via upload

---
+++ WEBSITE/ad_project_version_1/ad_project_version_1/JavaS.js
@@ -0,0 +1,11 @@
+﻿<script runat="server">
+
+    function redirectSearch()
+        {
+            var data = document.getElementById("searchText").value;
+    window.location = data;
+
+}
+
+
+    </script>
